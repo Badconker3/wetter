473f2f89c5e81e4ba0158396a631dc1c04b056da
Badconker3 <aarithefreak@msn.com>
2015-06-05 11:20:06 +0200

    geolocation bearbeitet

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
 		console.log(position);
+		$('.longitude').text(position.coords.longitude);
+		$('.latitude').text(position.coords.latitude);
+		$('.accuracy').text(position.coords.accuracy);
 	});
 });
