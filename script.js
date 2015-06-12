dea0071a8f951c14623d8f4461a34b20f2fff4c5
Badconker3 <aarithefreak@msn.com>
2015-06-12 11:08:37 +0200

    hello

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 				}
 			}).done(function(data){
 				console.log(data);
-				$('current')
+				$('current-location').text(data.results[0].formatted address);
 			})
 		});
 	});
