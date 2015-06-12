18a1671f56236a73ed1be7d8fbfeb85d05edd75f
Badconker3 <aarithefreak@msn.com>
2015-06-12 09:09:20 +0200

    Korrdinaten geändert

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
-		console.log(position);
-		$('.longitude').text(position.coords.longitude);
-		$('.latitude').text(position.coords.latitude);
-		$('.accuracy').text(position.coords.accuracy);
+		
+		var koordinaten = ({
+			longitude: position.coords.longitude,
+			latitude: position.coords.latitude
+		};
+		//console.log(position);
+		//$('.longitude').text(position.coords.longitude);
+		//$('.latitude').text(position.coords.latitude);
+		//$('.accuracy').text(position.coords.accuracy);
+		$.ajax{
+			url:'https://api.forecast.io/forecast/3a29abb9ce9c8e475af3d33012259689/' + koordinaten.latitude + ',' + koordinaten.longitude,
+			data: {
+				units: 'si',
+			(	lang: 'de'
+						},
+						dataType: 'jsonp'
+					}).done(function){
+				console.log(data);
+
+			});
 	});
 });
