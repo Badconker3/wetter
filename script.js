ba2cbab2560cc0e3c1e52b958dd5ae7bbede181c
Badconker3 <aarithefreak@msn.com>
2015-06-12 09:14:15 +0200

    Quelltext angepasst

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
 		$.ajax{
 			url:'https://api.forecast.io/forecast/3a29abb9ce9c8e475af3d33012259689/' + koordinaten.latitude + ',' + koordinaten.longitude,
 			data: {
-				units: 'si',
-			(	lang: 'de'
+			units: 'si',
+			lang: 'de'
 						},
 						dataType: 'jsonp'
 					}).done(function){
