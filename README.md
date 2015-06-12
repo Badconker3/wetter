1cdc18c994fe8923de2469838550e722a5b79faa
Badconker3 <aarithefreak@msn.com>
2015-06-12 09:27:33 +0200

    hello

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Wetterapplikation
+
+-Wetteranzeige jetzt und über den verlauf des Tages
+-Wettervorschau für die nächste Woche
+-aktuelle Radarsituation
+-animierte Wetter Situation
+-Meldung bei Unwetter (Sturmwarnung, Benachtrichtigung)
+-Kleidungs Vorschlag (z.B. heute wären FlipFlops gut oder heute warme Kleidung tragen)
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
-		
-		var koordinaten = ({
+
+	var koordinaten = {
 			longitude: position.coords.longitude,
 			latitude: position.coords.latitude
 		};
@@ -10,14 +10,14 @@ $(document).ready(function(){
 		//$('.latitude').text(position.coords.latitude);
 		//$('.accuracy').text(position.coords.accuracy);
 		$.ajax({
-			url:'https://api.forecast.io/forecast/3a29abb9ce9c8e475af3d33012259689/' + koordinaten.latitude + ',' + koordinaten.longitude,
-			data: {
-			units: 'si',
-			lang: 'de'
-						},
-						dataType: 'jsonp'
-					}).done(function(data)){
-				console.log(data);
+				url:'https://api.forecast.io/forecast/3a29abb9ce9c8e475af3d33012259689/' + koordinaten.latitude + ',' + koordinaten.longitude,
+				data: {
+					units: 'si',
+					lang: 'de'
+			},
+				dataType: 'jsonp'
+			}).done(function(data)){
+			console.log(data);
 
 		});
 	});
